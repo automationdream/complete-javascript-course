d73fac6443c5e5c9b38b90876204ce34761cf438
Automationdream <automationdream@gmail.com>
2020-05-23 19:28:38 +0200

    Variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,13 @@
-console.log("Hello ");
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 20;
+
+var fullAge = true;
+console.log(fullAge);
+
+job = 'Teacher';
+console.log(job);
+
+var e4years = 4;
