5d8a6178b6ea7fdf4508546d5ad3aefad4a36b4b
Automationdream <automationdream@gmail.com>
2020-05-23 20:06:58 +0200

    If else statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,64 +1,17 @@
-// Variable mutation 
+// If / else statements
+
 var firstName = 'John';
-var age = 28;
+var civilStatus = 'married';
 
-console.log(firstName + ' ' + age);
+if (civilStatus === 'married') {
+   console.log(firstName + ' is married ') 
+} else {
+   console.log(firstName + ' will hopefully marry soon') 
+}
 
-var job, isMarried;
-job = 'teacher';
-isMarried = true;
+var isMarried = true;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+if (isMarried) {
+    console.log('married')    
 
-// Variable mutation
-age = 'twenty eight';
-job = 'driver'
-
-// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
-// var lastName = prompt('What is his last name?')
-// console.log(firstName + ' ' + lastName)
-
-/**
- * Basic operators
- */
-var year, yearJohn, yearMark, ageJohn, ageMark;
-now = 2018;
-yearJohn = 2018 - 28;
-yearMark  = 2018 -33;
-
-console.log(yearJohn);
-
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 2);
-
-// Logical operators
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
-
-// typeof operator
-console.log(typeof johnOlder);
-
-// Multiple operators
-var now = 2018
-var yearJohn = 1989;
-var fullAge = 18;
-
-var isFullAge = now - yearJohn >= fullAge;
-console.log(isFullAge)
-
-// Grouping
-var ageJohn = now - yearJohn;
-var ageMark = 35;
-var average = (ageJohn + ageMark) / 2;
-console.log(average);
-
-// Multiple assignments
-var x,y;
-x = y = (3 + 5) * 4 - 6; 
-console.log(x,y);
-
-x *= 2;
-
-console.log(x);
+}
