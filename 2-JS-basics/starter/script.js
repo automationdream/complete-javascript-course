fe5ff0e07f9e65d540afe0743bd5f34f9116bb8a
Automationdream <automationdream@gmail.com>
2020-05-24 14:04:49 +0200

    continue in loop

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -4,9 +4,11 @@
  */
 
 var john = ['John', 'Smith', 1990, 'teacher', 'developer']
-var i = 0;
 
-while (i < john.length){
-    console.log(john[i]);
-    i++;
+for (var i=0; i < john.length;i++){
+    if (typeof john[i] !== 'string'){
+        continue; //continue if value not string
+    } else {
+     console.log(john[i])   
+    }
 }
