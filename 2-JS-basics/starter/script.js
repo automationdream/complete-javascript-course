4f19d423a83a386fb82cc4695a9be8a4a1f4ec9a
Automationdream <automationdream@gmail.com>
2020-05-24 13:03:49 +0200

    Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,21 +1,39 @@
-/*************************************
- * Function Statements and Expressions
+/********
+ * Arrays
  */
+// Initialize Array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990,1969,1948);
 
- // Function declaration
-// function whatDoYouDo(params) {}
+console.log(names[0]); //John
+console.log(names.length); //3
 
- // Function expression
-var whatDoYouDo = function (job, firstName) {
-    switch (job) {
-        case 'teacher':
-            return firstName + ' theaches kids how to code';
-        case 'developer':
-            return firstName + ' creates beautifull applications';
-        default:
-            return firstName + ' does something else';
-    }
+// Mutate array data
+names[1] = 'Ben'; //ammend value in array
+console.log(names);
 
-}
-console.log(whatDoYouDo('teacher', 'Bart'))
-console.log(whatDoYouDo('developer', 'Bart'))
+names[5] = 'Mary'; // John, Mark, Jane, empty, empty, Mary
+console.log(names);
+
+names[names.length] = "New name";
+console.log(names) 
+
+//  Arrays Different data types
+var john = ['John','Smith', 1990, 'teacher', 'developer']
+
+john.push('blue');
+console.log(john);
+
+john.unshift('Mr.') //Adds element on the beginning
+console.log(john);
+
+john.pop(); //rmoves last element
+console.log(john);
+
+john.shift(); //removes first element
+console.log(john);
+
+var check =john.indexOf(1990); //test if element exists in array => 2
+console.log(check);
+
+var isDesigner = john.indexOf('developer') === -1 ? console.log('john is not a developer') : console.log('john is a developer')
