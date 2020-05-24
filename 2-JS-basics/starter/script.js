7c2be24998f805e692cc01afc6f934a696dc6a15
Automationdream <automationdream@gmail.com>
2020-05-24 11:38:17 +0200

    Type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -6,9 +6,21 @@
 //truthy values: not falsy values
 
 var height;
+height = 23
 
 if (height) {
    console.log('Variable is definied') 
 } else {
    console.log('Variable is not definied') 
+}
+
+// Equality operators
+if (height == '23') {
+    // Uses type coercion
+    console.log('The operator == does type coercion');
+}
+
+if (height == '23') {
+    // Its an equality operation , does not match type coercion
+    console.log('The operator === matches the types does not use type coercion');
 }
