cf163775ed2f9ed59f1ef191787d851b99e05868
Automationdream <automationdream@gmail.com>
2020-05-24 11:51:19 +0200

    Functions

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,26 +1,29 @@
-/****************
- * Truthy and Falsy equality operators
+/**************
+ * Functions
+ *    
  */
 
-//falsy values: undefinied, null, 0, '', NaN
-//truthy values: not falsy values
-
-var height;
-height = 23
-
-if (height) {
-   console.log('Variable is definied') 
-} else {
-   console.log('Variable is not definied') 
+function calculateAge(birthYear) {
+    return 2020 - birthYear;
 }
 
-// Equality operators
-if (height == '23') {
-    // Uses type coercion
-    console.log('The operator == does type coercion');
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(birthYear, firstName) {
+   var age = calculateAge(birthYear); 
+   var retirement = 65 - age;
+
+   if (retirement<=0){
+       console.log(firstName + ' is already in retirement ');
+   } else {
+       console.log(firstName + ' retires in ' + retirement + ' years.');
+   }
 }
 
-if (height == '23') {
-    // Its an equality operation , does not match type coercion
-    console.log('The operator === matches the types does not use type coercion');
-}
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'John');
