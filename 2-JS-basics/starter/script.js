4826151d6dbcefe6b8073b02580760459511bb67
Automationdream <automationdream@gmail.com>
2020-05-24 13:43:26 +0200

    basic for loop

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,20 +1,10 @@
 /*********************************
- * Objects and methods 
+ * Loops and iteration
  * 
  */
 
-var john = {
-    firstName: 'John',
-    lastName: 'Smith',
-    birthYear: 1990,
-    family: ['Jane', 'John'],
-    job: 'teacher',
-    isMarried: true,
-    calcAge: function (birthYear) {
-        return 2018 - this.birthYear; //using this
-    }
- 
-};
-
-john.age = john.calcAge(); // using builtin function and adding the new index into the object
-console.log(john)
+ for (var i = 0; i < 10 ; i++) {
+     
+    console.log(i);
+     
+ }
