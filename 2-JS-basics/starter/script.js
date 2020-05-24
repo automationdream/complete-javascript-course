65342776433663b27c8db1fabe4915a5bac7f48b
Automationdream <automationdream@gmail.com>
2020-05-24 14:06:00 +0200

    break

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -7,7 +7,7 @@ var john = ['John', 'Smith', 1990, 'teacher', 'developer']
 
 for (var i=0; i < john.length;i++){
     if (typeof john[i] !== 'string'){
-        continue; //continue if value not string
+        break; // exits the entire iteration
     } else {
      console.log(john[i])   
     }
