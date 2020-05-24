02b6ddf2df125cc106515b325fcc6ae3812e4515
Automationdream <automationdream@gmail.com>
2020-05-24 13:31:27 +0200

    Objects

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,39 +1,26 @@
-/********
- * Arrays
+/*************************
+ * Objects and properties
  */
-// Initialize Array
-var names = ['John', 'Mark', 'Jane'];
-var years = new Array(1990,1969,1948);
 
-console.log(names[0]); //John
-console.log(names.length); //3
 
-// Mutate array data
-names[1] = 'Ben'; //ammend value in array
-console.log(names);
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'John'],
+    job: 'teacher',
+    isMarried: true,
 
-names[5] = 'Mary'; // John, Mark, Jane, empty, empty, Mary
-console.log(names);
+}
 
-names[names.length] = "New name";
-console.log(names) 
+console.log(john.firstName); //accessing
+console.log(john['firstName']); //accessing
 
-//  Arrays Different data types
-var john = ['John','Smith', 1990, 'teacher', 'developer']
+var x = 'birthYear';
+console.log(john[x]);
 
-john.push('blue');
-console.log(john);
+var jane = new Object();
 
-john.unshift('Mr.') //Adds element on the beginning
-console.log(john);
-
-john.pop(); //rmoves last element
-console.log(john);
-
-john.shift(); //removes first element
-console.log(john);
-
-var check =john.indexOf(1990); //test if element exists in array => 2
-console.log(check);
-
-var isDesigner = john.indexOf('developer') === -1 ? console.log('john is not a developer') : console.log('john is a developer')
+//New Object syntax
+jane.firstName = 'Jane';
+jane.lastName = 'Smith';
