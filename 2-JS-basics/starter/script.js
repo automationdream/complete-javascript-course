87e8012f1c18baf78c544e2d53609cdfb3f94fa7
Automationdream <automationdream@gmail.com>
2020-05-24 11:34:19 +0200

    Truthy and falsy values

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,27 +1,14 @@
-// Ternary operator
+/****************
+ * Truthy and Falsy equality operators
+ */
 
-var firstName = 'John';
-var age = 16;
+//falsy values: undefinied, null, 0, '', NaN
+//truthy values: not falsy values
 
-age >= 18 ? console.log(firstName + ' drinks beer.') 
-: console.log(firstName + " drinks juice.");
+var height;
 
-
-var drink = age >= 18 ? console.log('beer'): console.log('juice');
-console.log(drink);
-
-// Switch statement
-job = 'teacher'
-switch (job) {
-    case 'teacher':
-        console.log('teatcher')
-        // If there would not be a break then it will test next statements 
-        break;
-
-    case 'driver':
-        console.log('teatcher')
-        
-        break;
-    default:
-        break;
+if (height) {
+   console.log('Variable is definied') 
+} else {
+   console.log('Variable is not definied') 
 }
