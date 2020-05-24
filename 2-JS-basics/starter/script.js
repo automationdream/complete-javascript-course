7c31ee92a27e0e3781a24413e26a4fbef9926e54
Automationdream <automationdream@gmail.com>
2020-05-24 14:09:54 +0200

    reverse loop

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -4,10 +4,10 @@
  */
 
 var john = ['John', 'Smith', 1990, 'teacher', 'developer']
-
-for (var i=0; i < john.length;i++){
+var lenght_of_an_array = john.length -1
+for (var i = lenght_of_an_array; i >= 0;i--){
     if (typeof john[i] !== 'string'){
-        break; // exits the entire iteration
+        continue; // exits the entire iteration
     } else {
      console.log(john[i])   
     }
