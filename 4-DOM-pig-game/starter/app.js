e020da2c000890b145e0a3a56f2d6e4c03ba8520
Automationdream <automationdream@gmail.com>
2020-05-25 13:59:52 +0200

    toggle

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -40,8 +40,9 @@ function rollDice() {
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
 
-        document.querySelector('.player-0-panel').classList.remove('active');
         
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
     }
 }
 
