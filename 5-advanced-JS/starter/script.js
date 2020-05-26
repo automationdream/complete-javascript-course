475032676efd171655cef93ec5a7426f1efff0a6
Automationdream <automationdream@gmail.com>
2020-05-26 12:54:04 +0200

    more inheriting

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -10,8 +10,14 @@ Person.prototype.calculateAge = function (currentYear) {
         console.log(currentYear - this.yearOfBirth)
     }
 
+Person.prototype.lastName = 'Smith' 
+
 var john = new Person('John', 1990, 'teacher');
 var jane = new Person('John', 1969, 'disigner');
+var mark = new Person('Mark', 1989, 'retired');
 
 john.calculateAge(currentYear);
 jane.calculateAge(currentYear);
+mark.calculateAge(currentYear);
+
+console.log(john.lastName)
