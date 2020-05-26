757cb9e053e46e85c037cc88b784ce32bb047fab
Automationdream <automationdream@gmail.com>
2020-05-26 12:46:35 +0200

    inheritance

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -2,6 +2,11 @@ var Person = function (name, yearOfBirth, job) {
     this.name = name,
     this.yearOfBirth = yearOfBirth,
     this.job = job
+    this.calculateAge = function (currentYear) {
+        console.log(currentYear - this.yearOfBirth)
+    }
 }
 
 var john = new Person('John', 1990, 'teacher');
+
+john.calculateAge(2020);
