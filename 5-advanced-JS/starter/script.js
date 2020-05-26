754e360a766f6dd28707b0880cac90f57be2cbdf
Automationdream <automationdream@gmail.com>
2020-05-26 12:51:28 +0200

    prototyping in object

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -4,10 +4,11 @@ var Person = function (name, yearOfBirth, job) {
     this.name = name,
     this.yearOfBirth = yearOfBirth,
     this.job = job
-    this.calculateAge = function (currentYear) {
+}
+
+Person.prototype.calculateAge = function (currentYear) {
         console.log(currentYear - this.yearOfBirth)
     }
-}
 
 var john = new Person('John', 1990, 'teacher');
 var jane = new Person('John', 1969, 'disigner');
