1c77d708787205d151570a06b04bcc9d669e45de
Bartosz Tyrala <automationdream@gmail.com>
2020-05-30 17:36:41 +0200

    DRY

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -7,13 +7,23 @@ var budgetController = (
 
 var UIController = (function () {
 
+        var DOMstrings = {
+            inputType: '.add__type',
+            inputDesription: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn',
+        };
+
     return {
         getinput: function () {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value,
-            }
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDesription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     }
    // some code 
@@ -22,8 +32,12 @@ var UIController = (function () {
 )();
 
 var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem  = function() {
         
+
         // 1. Get the filed input data
         var input = UICtrl.getinput();
         console.log(input);
@@ -36,7 +50,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
