2160b2f7b991df175ed2a2df343e1b8247ce4eaf
Bartosz Tyrala <automationdream@gmail.com>
2020-05-31 13:42:06 +0200

    preparing template for replacement

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -68,6 +68,23 @@ var UIController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value,
             };
         },
+        addListItem: function (obj,type){
+            var html, newHtml; 
+
+           // Create HTML string with placeholder text 
+
+           if (type === 'inc'){
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+           } else if (type === 'exp'){
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+           }
+           // Replace placeholder text with some actual data
+           newHtml = html.replace('%id%', obj.id);
+           newHtml = newHtml.replace('%description%', obj.description);
+           newHtml = newHtml.replace('%value%', obj.value);
+           // Insert HTML into DOM
+
+        },
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -98,10 +115,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 1. Get the filed input data
         var input = UICtrl.getinput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
-        budgetController.addItem(input.type, input.description, input.value);
+        newItem = budgetController.addItem(input.type, input.description, input.value);
 
         // 3. Calculate the budget
 
