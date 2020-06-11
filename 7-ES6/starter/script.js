e202a334706bdecae289475ef69bf22cfdcf07ca
Bartosz Tyrala <automationdream@gmail.com>
2020-06-11 20:04:20 +0200

    Array ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -75,3 +75,17 @@ Person.prototype.myFriends = (friends) => {
 var friends = ['Bob', 'Jane', 'Mark']
 
 new Person('John').myFriends(friends);
+
+const boxes = document.querySelectorAll('.box');
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+})
+
+const boxesArr6 = Array.from(boxes);
+
+boxesArr6.forEach(cur => {
+    cur.style.backgroundColor = 'green';
+})
