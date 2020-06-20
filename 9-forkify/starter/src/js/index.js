4353527cc66d26195d856a0ed754b18573717ce8
Bartosz Tyrala <automationdream@gmail.com>
2020-06-20 17:05:44 +0200

    sending get request with axios

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,9 @@
 import axios from 'axios';
+
+async function getResults(query) {
+    const result = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    console.log(result)
+
+}
+
+getResults('pizza')
