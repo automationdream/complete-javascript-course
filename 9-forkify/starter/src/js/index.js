80180afc8458fa956fd3e2a6f4d962d9ec4deb5a
Bartosz Tyrala <automationdream@gmail.com>
2020-06-20 17:37:36 +0200

    constructing class with axios get

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search'
 
-async function getResults(query) {
-    try {
-            const result = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-            const recipes = result.data.recipes;
-            console.log(recipes)
-    }
-    catch (error) {
-        alert(error)
-    }
-
-}
-
-getResults('dupa')
+const search = new Search('pizza')
+console.log(search);
+search.getResults()
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an exported string';
+import axios from 'axios';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+        async getResults() {
+            try {
+                    const result = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+                    this.result = result.data.recipes;
+                    // console.log(this.result)
+            }
+            catch (error) {
+                alert(error)
+            }
+
+}
+    }
+
+
