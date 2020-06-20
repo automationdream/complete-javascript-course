f5657f17bf7f32e9379d66c8a48bdcc6d02e8dc1
Bartosz Tyrala <automationdream@gmail.com>
2020-06-20 18:07:21 +0200

    Blueprint of new application

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,31 @@
 import Search from './models/Search'
 
-const search = new Search('pizza')
-console.log(search);
-search.getResults()
+/** Global state of the app
+ * - Search object
+ * - Current reipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {}
+const controlSearch = async () => {
+    //1) Get query from view
+    const query = 'pizza' //TODO
+
+    //2) New search object and state
+    state.search = new Search(query);
+    // search.getResults()
+
+    //3) Prepare UI for results
+
+    //4) Search for recipes
+    await state.search.getResults();
+
+    //5) Render result on UI
+    console.log(state.search.result);
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch()
+})
